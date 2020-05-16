132ae6e1bddcf6610725408049678e43ac64e165
Ronney Ismael <ronneyismael@Ronneys-MacBook-Air.local>
2020-05-16 11:01:11 +0200

    third commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'employee-crud-frontend';
+  navLinks: any[];
+  activeLinkIndex = -1;
+ 
+  constructor(private router: Router) {
+    this.navLinks = [
+        {
+            label: 'Employee List',
+            link: '/employee-list',
+            index: 0
+        }, {
+            label: 'Add Employee',
+            link: '/add-employee',
+            index: 1
+        }, 
+    ];
+}
+
+
+ngOnInit(): void {
+  this.router.events.subscribe((res) => {
+
+      this.activeLinkIndex = this.navLinks.indexOf(this.navLinks.find(tab => tab.link === this.router.url));
+  });
+}
 }
